fe104a00bd160574a049d8405ca164e8853202ad
Your Name <you@example.com>
2023-09-05 03:43:55 +0800

    add test foxtool

--- app/foxtool/page.js
+++ app/foxtool/page.js
@@ -1,31 +1,54 @@
 "use client"
 import React, { useState } from 'react';
 
-
-const getdata = (urlword) => {
-
+const getnewdata=(val,qian,zhong,hou)=>{
+  return  {
+    getval:val*qian/hou,
+    count:val *(zhong - qian),
+    countt:val *(zhong - qian)*0.5,
+    soul:val *(hou-zhong),
+  } 
 }
 
+
 export default function Home() {
-  const [value, onChangeText] = useState({
+/*   const [value, onChangeText] = useState({
+    val:'',
     qian:'',
-    hou:'',
+    zhong:'',
+    hou:''
+    
+  }); */
+  const [val,getval]=useState('');
+  const [qian,getqian]=useState('');
+  const [zhong,getzhong]=useState('');
+  const [hou,gethou]=useState('');
+  const [sum, setSum] = useState({
+    getval:'',
+    count:'',
+    countt:'',
+    soul:''
   });
-  const [sum, setSum] = useState('');
   
   return (
     <div>
-      <div><h1>{sum}</h1></div>
-      <div><input type="text" name="qian"   placeholder="Type here" className="input input-bordered w-full max-w-xs" 
-        value={value.qian} /></div>
-      <div><input type="text" name="hou"  placeholder="Type here" className="input input-bordered w-full max-w-xs"
-        value={value.hou} /></div>
+      <div><label>value:</label><h1>{sum.getval}</h1></div>
+      <div><label>count:</label><h1>{sum.count}</h1></div>
+      <div><label>50%count:</label><h1>{sum.countt}</h1></div>
+      <div><label>soul:</label><h1>{sum.soul}</h1></div>
+      <div><input type="text" name="qian"   placeholder="Type here" className="input input-bordered w-full max-w-xs" onChange={e => getval(e.target.value)}
+        value={val} /></div>
+      <div><input type="text" name="hou"  placeholder="Type here" className="input input-bordered w-full max-w-xs" onChange={e => getqian(e.target.value)}
+        value={qian} /></div>
+      <div><input type="text" name="hou"  placeholder="Type here" className="input input-bordered w-full max-w-xs" onChange={e => getzhong(e.target.value)}
+        value={zhong} /></div>
+      <div><input type="text" name="hou"  placeholder="Type here" className="input input-bordered w-full max-w-xs" onChange={e => gethou(e.target.value)}
+        value={hou} /></div>
       <div>
         <button className="btn btn-success"
           onClick={() => {
-          console.log(value)
-         //  let test=parseInt(value.qian)+parseInt(value.hou);
-            setSum(value.qian)
+            const newdata=getnewdata(val,qian,zhong,hou)
+            setSum(newdata)
           }}
         >sum</button>
       </div>
