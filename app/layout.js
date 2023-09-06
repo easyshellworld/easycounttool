1d5f558290cb121e5d9a4442d5eab0673c8c4a28
shell <shell@thisworld.club>
2023-09-06 21:13:15 +0800

    add gettool pages

--- app/layout.js
+++ app/layout.js
@@ -30,8 +30,8 @@ export default function RootLayout({ children }) {
       <a className="btn btn-ghost normal-case text-xl">Easy count tool</a>
     </div>
     <div className="navbar-end">
-      <button className="btn btn-ghost btn-circle">
-        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
+      <button className="btn btn-ghost btn-circle" /* onClick={()=>{window.location.href = 'https://unbug.github.io/codelf'}} */>
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>  
       </button>
       <button className="btn btn-ghost btn-circle">
         <div className="indicator">
