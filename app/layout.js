68b62ef60dc05ef05b4b543b0e3716916e384e11
shell <shell@thisworld.club>
2023-09-10 13:53:24 +0800

    add codelf quickref button

--- app/layout.js
+++ app/layout.js
@@ -13,7 +13,7 @@ export const metadata = {
 }
 
 export default function RootLayout({ children }) {
-  const router = useRouter()
+  
 
   return (
     <html lang="en">
@@ -35,16 +35,15 @@ export default function RootLayout({ children }) {
       <a className="btn btn-ghost normal-case text-xl">Easy count tool</a>
     </div>
     <div className="navbar-end">
-      <button className="btn btn-ghost btn-circle" onClick={()=>{
-        router.push('https://unbug.github.io/codelf')}}>
+    <Link className="btn btn-ghost btn-circle" href="https://unbug.github.io/codelf">
         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>  
-      </button>
-      <button className="btn btn-ghost btn-circle">
+      </Link>
+      <Link className="btn btn-ghost btn-circle" href="https://quickref.cn/">
         <div className="indicator">
           <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" /></svg>
           <span className="badge badge-xs badge-primary indicator-item"></span>
         </div>
-      </button>
+      </Link>
     </div>
   </div>
       <div className={inter.className}>{children}</div>
