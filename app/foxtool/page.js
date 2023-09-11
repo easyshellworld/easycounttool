412cc238dc56f90aa21b2a73395bb0af6801336b
easyshellworld <122170820+easyshellworld@users.noreply.github.com>
2023-09-11 15:07:09 +0800

    Update page.js change count for u

--- app/foxtool/page.js
+++ app/foxtool/page.js
@@ -5,8 +5,11 @@ const getnewdata=(val,qian,zhong,hou)=>{
   return  {
     getval:val*qian/hou,
     count:val *(zhong - qian),
+    countu:val-val*qian/zhong,
     countt:val *(zhong - qian)*0.5,
+    counttu:(val-val*qian/zhong)*0.5,
     soul:val *(hou-zhong),
+    soulu:val*qian/zhong-val*qian/hou
   } 
 }
 
@@ -32,10 +35,10 @@ export default function Home() {
   
   return (
     <div className="grid grid-cols-0 gap-8 place-content-center">
-      <div><label>value:</label><h1>{sum.getval}</h1></div>
-      <div><label>count:</label><h1>{sum.count}</h1></div>
-      <div><label>50%count:</label><h1>{sum.countt}</h1></div>
-      <div><label>soul:</label><h1>{sum.soul}</h1></div>
+      <div><label>value:</label>{sum.getval}</div>
+      <div><label>count:</label>{sum.count}<label> countu:</label>{sum.countu}</div>
+      <div><label>50%count:</label>{sum.countt}<label> counttu:</label>{sum.counttu} </div>
+      <div><label>soul:</label>{sum.soul} <label> soulu:</label>{sum.soulu} </div>
       <div><input type="text" name="qian"   placeholder="Type here" className="input input-bordered w-full max-w-xs" onChange={e => getval(e.target.value)}
         value={val} /></div>
       <div><input type="text" name="hou"  placeholder="Type here" className="input input-bordered w-full max-w-xs" onChange={e => getqian(e.target.value)}
